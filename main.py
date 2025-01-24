bad1de32cb1038ccf1dcf206f1010460d4faabf2
atrox3d <atrox3d@gmail.com>
2025-01-24 10:03:14 +0100

    add --match --case --path

--- main.py
+++ main.py
@@ -9,16 +9,18 @@ app = typer.Typer()
 
 
 @app.callback(invoke_without_command=True)
-def default(ctx:typer.Context):
+def default(ctx:typer.Context, path:str='data', match:str='', case:bool=False):
     ctx.ensure_object(dict)
+    
     print('STARTED')
-    list_files()
+    
+    list_files(path, match, case)
 
 
 @app.command('list')
-def list_files(path:str='data'):
+def list_files(path:str='data', match:str='', case:bool=False):
     files_paths = list(Path(path).glob('*.xls*'))
-    print([file.name for file in files_paths])
+    print([file.name for file in files_paths if (match if case else match.lower()) in (file.name if case else file.name.lower())])
 
 
 
