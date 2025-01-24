fa4325d680d70fbb8fcd3996198f6c99d1fd6163
atrox3d <atrox3d@gmail.com>
2025-01-24 10:06:03 +0100

    add --sort

--- main.py
+++ main.py
@@ -9,18 +9,18 @@ app = typer.Typer()
 
 
 @app.callback(invoke_without_command=True)
-def default(ctx:typer.Context, path:str='data', match:str='', case:bool=False):
+def default(ctx:typer.Context, path:str='data', match:str='', case:bool=False, sort:bool=True):
     ctx.ensure_object(dict)
     
     print('STARTED')
     
-    list_files(path, match, case)
+    list_files(path, match, case, sort)
 
 
 @app.command('list')
-def list_files(path:str='data', match:str='', case:bool=False):
+def list_files(path:str='data', match:str='', case:bool=False, sort:bool=True):
     files_paths = list(Path(path).glob('*.xls*'))
-    [print(file.name) for file in files_paths if (match if case else match.lower()) in (file.name if case else file.name.lower())]
+    [print(file.name) for file in (files_paths if not sort else sorted(files_paths)) if (match if case else match.lower()) in (file.name if case else file.name.lower())]
 
 
 
