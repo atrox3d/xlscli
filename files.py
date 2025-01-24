aec0575576444aae689ac696c58df89b7ed6f68d
atrox3d <atrox3d@gmail.com>
2025-01-24 11:53:06 +0100

    add choose and abort

--- files.py
+++ files.py
@@ -29,3 +29,24 @@ def list_files(path:str='data', match:str='', case:bool=False, sort:bool=True):
     ]
     for n, file in enumerate(file_list, 1):
         print(f'({n:2}) - {file!r}')
+
+
+@app.command('choose')
+def choose_file(path:str='data', match:str='', case:bool=False, sort:bool=True):
+    # print(locals())
+    # exit()
+    files_paths = list(Path(path).glob('*.xls*'))
+    file_list = [
+        file.name
+        for file in (files_paths if not sort else sorted(files_paths))
+        if (match if case else match.lower()) in (file.name if case else file.name.lower())
+    ]
+    for n, file in enumerate(file_list, 1):
+        print(f'({n:2}) - {file!r}')
+    
+    try:
+        fileno = int(input('choose file number: '))
+        print(f'you have chosen {fileno}: {file_list[fileno-1]}')
+    except IndexError:
+        print(f'wrong number {fileno}')
+        raise typer.Abort()
