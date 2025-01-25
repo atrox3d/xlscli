ef7c46d4a2ccb14daace1d516a812e91ba48ef31
atrox3d <atrox3d@gmail.com>
2025-01-25 16:27:14 +0100

    optimize files

--- commands/files.py
+++ commands/files.py
@@ -2,42 +2,43 @@ import logging
 import typer
 from pathlib import Path
 
-from helpers import files
+# from helpers import files
 from helpers import xls
-from helpers import config
+# from helpers import config
 
 
 logger = logging.getLogger(__name__)
 
-app = typer.Typer()
+# app = typer.Typer()
 
 
-@app.callback(invoke_without_command=True)
-def default(
-        ctx:typer.Context,
-        path:str=config.input_dir(),
-        match:str='',
-        case:bool=False,
-        sort:bool=True
-):
-    ctx.ensure_object(dict)
-    
-    logger.debug(f'files callback STARTED {ctx.invoked_subcommand = }')
-    if ctx.invoked_subcommand is None:
-        list_files(path, match, case, sort)
+# @app.callback(invoke_without_command=True)
+# def default(
+        # ctx:typer.Context,
+        # path:str=config.input_dir(),
+        # match:str='',
+        # case:bool=False,
+        # sort:bool=True
+# ):
+    # ctx.ensure_object(dict)
+    # 
+    # logger.debug(f'files callback STARTED {ctx.invoked_subcommand = }')
+    # if ctx.invoked_subcommand is None:
+        # list_files(path, match, case, sort)
 
 
-@app.command('list')
+# @app.command('list')
 def list_files(
         path:str,
         match:str='',
         case:bool=False,
-        sort:bool=True
+        sort:bool=True,
+        reverse:bool=False
 ):
     files_paths = list(Path(path).glob('*.xls*'))
     file_list = [
         file.name
-        for file in (files_paths if not sort else sorted(files_paths))
+        for file in (files_paths if not sort else sorted(files_paths, reverse=reverse))
         if (match if case else match.lower()) in (file.name if case else file.name.lower())
     ]
     # for n, file in enumerate(file_list, 1):
@@ -45,7 +46,8 @@ def list_files(
     for file in file_list:
         print(file)
 
-@app.command('open')
+
+# @app.command('open')
 def open_file(
         filename:str,
         path:str=None,
