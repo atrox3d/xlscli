bdc5f8babdfbf0ae040ad3c4b071a6bb335ae744
atrox3d <atrox3d@gmail.com>
2025-01-25 05:44:59 +0100

    checking for options from file

--- main.py
+++ main.py
@@ -16,17 +16,14 @@ app.add_typer(files.app, name='files')
 @app.callback(invoke_without_command=True)
 def default(
     ctx:typer.Context, 
-    path:str=config.data_dir(), match:str='', case:bool=False, sort:bool=True,
+    path:str=config.data_dir(), match:str='', case:bool=False, sort:bool=True,  # list params
     log_level:logconfig.LogLevels = 'INFO'
 ):
     '''***DEFAULT ACTION WITH NO ARGUMENTS IS THE LIST COMMAND***'''
     
-    logging.basicConfig(
-        level=log_level.value
-    )
-    
+    logging.basicConfig(level=log_level.value)
+    print(f'{ctx.default_map = }')
     ctx.ensure_object(dict)
-    
     logger.debug(f'main callback STARTED {ctx.invoked_subcommand = }')
     if ctx.invoked_subcommand is None:
         files.list_files(path, match, case, sort)
