00eb1d1800c3f79be6c107a4fc66a7b7ae55aab2
atrox3d <atrox3d@gmail.com>
2025-01-25 04:53:25 +0100

    create helpers.files

---
+++ helpers/files.py
@@ -0,0 +1,23 @@
+import typer
+
+
+from pathlib import Path
+
+
+def choose_file(path:str='data', match:str='', case:bool=False, sort:bool=True) -> str:
+    files_paths = list(Path(path).glob('*.xls*'))
+    file_list = [
+        file.name
+        for file in (files_paths if not sort else sorted(files_paths))
+        if (match if case else match.lower()) in (file.name if case else file.name.lower())
+    ]
+    for n, file in enumerate(file_list, 1):
+        print(f'({n:2}) - {file!r}')
+
+    try:
+        fileno = int(input('choose file number: '))
+        print(f'you have chosen {fileno}: {file_list[fileno-1]}')
+        return file_list[fileno-1]
+    except IndexError:
+        print(f'wrong number {fileno}')
+        raise typer.Abort()
