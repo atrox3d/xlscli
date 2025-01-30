efbb32636e3ac0b4616c7f35ac194ee1eabdf001
atrox3d <atrox3d@gmail.com>
2025-01-30 10:45:31 +0100

    move get_files to helpers.files

--- commands/files.py
+++ commands/files.py
@@ -2,44 +2,11 @@ import logging
 import typer
 from pathlib import Path
 
+from helpers.files import get_files
 from helpers import xls
 
 
 logger = logging.getLogger(__name__)
-
-# app = typer.Typer()
-
-
-# @app.callback(invoke_without_command=True)
-# def default(
-        # ctx:typer.Context,
-        # path:str=config.input_dir(),
-        # match:str='',
-        # case:bool=False,
-        # sort:bool=True
-# ):
-    # ctx.ensure_object(dict)
-    # 
-    # logger.debug(f'files callback STARTED {ctx.invoked_subcommand = }')
-    # if ctx.invoked_subcommand is None:
-        # list_files(path, match, case, sort)
-
-
-def get_files(
-        path:str,
-        match:str='',
-        case:bool=False,
-        sort:bool=True,
-        reverse:bool=False
-) -> list[str]:
-    files_paths = list(Path(path).glob('*.xls*'))
-    file_list = [
-        file.name
-        for file in (files_paths if not sort else sorted(files_paths, reverse=reverse))
-        if (match if case else match.lower()) in (file.name if case else file.name.lower())
-    ]
-    return file_list
-
 
 # @app.command('list')
 def list_files(
---
+++ helpers/files.py
@@ -0,0 +1,35 @@
+# app = typer.Typer()
+
+
+# @app.callback(invoke_without_command=True)
+# def default(
+        # ctx:typer.Context,
+        # path:str=config.input_dir(),
+        # match:str='',
+        # case:bool=False,
+        # sort:bool=True
+# ):
+    # ctx.ensure_object(dict)
+    # 
+    # logger.debug(f'files callback STARTED {ctx.invoked_subcommand = }')
+    # if ctx.invoked_subcommand is None:
+        # list_files(path, match, case, sort)
+
+
+from pathlib import Path
+
+
+def get_files(
+        path:str,
+        match:str='',
+        case:bool=False,
+        sort:bool=True,
+        reverse:bool=False
+) -> list[str]:
+    files_paths = list(Path(path).glob('*.xls*'))
+    file_list = [
+        file.name
+        for file in (files_paths if not sort else sorted(files_paths, reverse=reverse))
+        if (match if case else match.lower()) in (file.name if case else file.name.lower())
+    ]
+    return file_list
