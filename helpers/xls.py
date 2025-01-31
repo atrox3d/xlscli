e7151ca4838c470fd2b18227b08f756fa218cea9
atrox3d <atrox3d@gmail.com>
2025-01-31 10:15:42 +0100

    add sheet selection

--- helpers/xls.py
+++ helpers/xls.py
@@ -7,14 +7,35 @@ import typer
 logger = logging.getLogger(__name__)
 
 
-def open_xls(path:Path) -> pandas.DataFrame:
-    workbook = pandas.read_excel(path, sheet_name=None)
-    for sheet, df in workbook.items():
+def print_sheet(sheet:str, df:pandas.DataFrame):
         print()
         print(sheet)
         print()
         print(df.to_string(index=False))
         print()
+
+
+def ask_sheet(workbook:dict) -> str|None:
+    if len(workbook) > 1:
+        for n, sheet in enumerate(workbook):
+            print(f'({n:2}) - {sheet!r}')
+        choice = input('choose sheet number (enter=all): ')
+        print(f'{choice!r}')
+        
+        return list(workbook.keys())[int(choice)] if choice else None
+    else:
+        return None
+
+
+def open_xls(path:Path) -> pandas.DataFrame:
+    workbook = pandas.read_excel(path, sheet_name=None)
+    
+    if (sheet :=ask_sheet(workbook)) is None:
+        for sheet, df in workbook.items():
+            print_sheet(sheet, df)
+    else:
+        print_sheet(sheet, workbook[sheet])
+
     print(f'total sheets: {len(workbook)}')
     print(f'sheets: {list(workbook.keys())}')
 
