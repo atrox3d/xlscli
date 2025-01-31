8e872dc06282822c7d367c7b886dc0c559427019
atrox3d <atrox3d@gmail.com>
2025-01-31 18:27:51 +0100

    setup tests and write first tests

---
+++ tests/helpers/test_config.py
@@ -0,0 +1,25 @@
+from tempfile import TemporaryDirectory
+from pathlib import Path
+import json
+
+from helpers import config
+
+
+def test_config_load_defaults():
+    assert config._config == config.load('wrong')
+
+
+def test_fake_config():
+    with TemporaryDirectory() as tempdir:
+        fakedata = {
+            'input_dir': 'inputdir',
+            'output_dir': 'outputdir'
+        }
+        fakeconfig_path = str(Path(tempdir, 'config.json'))
+        with open(fakeconfig_path, 'w') as fp:
+            json.dump(fakedata, fp)
+        
+        data = config.load(fakeconfig_path)
+        assert data == fakedata
+
+
