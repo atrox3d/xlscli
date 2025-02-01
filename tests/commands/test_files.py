09451c59f0df409d2bccd85d9e2f5ff94e826ab1
atrox3d <atrox3d@gmail.com>
2025-02-01 11:44:24 +0100

    work with patch print

--- tests/commands/test_files.py
+++ tests/commands/test_files.py
@@ -1,5 +1,19 @@
 import pytest
-from unittest.mock import patch
+from unittest.mock import patch, call
 
 from commands import files
+
+
+def test_list_files(
+    # mockprint, 
+    temp_dir, 
+    fake_files, 
+    fake_xls_filenames
+):
+    with patch('builtins.print') as mockprint:
+        files.list_files(temp_dir)
+        calls = mockprint.mock_calls
+        assert calls == [call(f) for f in fake_xls_filenames]
+    print(calls)
+
 
